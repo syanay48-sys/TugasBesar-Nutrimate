9afb103d36e9f1007e0090c7deb4b890d0dcc87a
Nayla <syanay48@gmail.com>
2026-06-14 21:10:52 +0700

    Progress Aplikasi Tugas Besar 2

---
+++ src/NutriMate/UserDataSession.java
@@ -0,0 +1,7 @@
+ package NutriMate;
+ 
+public class UserDataSession {
+    public static String namaUser = "Vira";
+    public static double targetKalori = 2000;
+    public static double beratBadan = 70;
+}
